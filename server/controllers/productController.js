a28b8031a1e922ec1ab4dbd5eb2898fef7d8b624
Ardia Aprireo I. <aprireo@gmail.com>
2023-10-27 18:56:54 +0700

    work on product (both controllers and routes)

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,15 +1,49 @@
-const fs = require('fs')
-const path = require('path')
-const dbPath = path.join(__dirname, '../migrations/20231027090611_products.js')
+const Product = require('../migrations/20231027090611_products')
 
 const productController = {
     getAll : async (req, res) => {
         try {
+        const products = await Product.find()
         
+        return res.status(200).json(products)
+        } catch (error) {
+        return res.status(500).json({message: 'An error occured while fetching products'})
+        }
+    },
+
+    createProduct : async (req, res) => {
+        const {name, rent, date, status} = req.body
+        try {
+         const products = await Product.create({
+            name, rent, date, status
+         })
+            
+         return res.status(200).json(products)
+        } catch (error) {
+         return res.status(500).json({message: 'An error occured while fetching products'})
+         }
+        },
+
+    editProduct : async (req, res) => {
+        const {id} = req.params
+        const {name, rent, date, status} = req.body
+        try {
+            const patch = await Product.update({
+                name, rent, date, status
+            }, {where: {id: id}})
+
+            return res.status(200).json(patch)
+        } catch (error) {
+            return res.status(500).json({message: 'An error occured while edit products'})
+         }
+        },
+    deleteProduct : async (req, res) => {
+        try {
+            
         } catch (error) {
             
         }
     }
-}
+    }
 
 module.exports = productController
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,4 +2,12 @@ const express = require("express")
 const router = express.Router()
 const productController = require("../controllers").productController
 
+router.get('/viewallitem', productController.getAll)
+
+router.post('/additem', productController.createProduct)
+
+router.patch('/edititem', productController.editProduct)
+
+router.delete('/deleteitem', productController.deleteProduct)
+
 module.exports = router
